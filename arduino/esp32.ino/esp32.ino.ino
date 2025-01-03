d8712307ae1bdbbd3650634db846443f5080ac21
snakex64 <pascx64@gmail.com>
2025-01-02 22:36:30 -0500

    First commit

---
+++ arduino/esp32.ino/esp32.ino.ino
@@ -0,0 +1,120 @@
+// GPIO list: https://i.ibb.co/BKn4dgL/ESP32-DEV-CP2102-C-003.jpg
+
+#include <Arduino.h>
+#include <Encoder.h>
+#include <Wire.h>
+
+// ----------------------------------------------------------------------------------- Pin Configurations
+Encoder encoder_left_front(36, 39);
+Encoder encoder_left_back(34, 35);
+Encoder encoder_right_front(32, 33);
+Encoder encoder_right_back(25, 26);
+
+const int motorSpeedLeftPin = 27; // PWM-capable pin
+const int motorSpeedRightPin = 14; // PWM-capable pin
+const int motorDirectionLeftAPin = 12;
+const int motorDirectionLeftBPin = 13;
+const int motorDirectionRightAPin = 23;
+const int motorDirectionRightBPin = 22;
+// ----------------------------------------------------------------------------------- Settings
+const int frequency = 30000; // PWM frequency (30 kHz) 
+const int resolution = 8; // Must be changed in "updateSpeedController" if this is modified
+
+void setup() 
+{
+  Wire.begin(2); // Setup i2c slave using pin 21 (SCL) and 22(SDA) and #2 address
+  Wire.onRequest(requestEvent); // register event
+  Wire.onReceive(receiveEvent); // register event
+
+  // Configure PWM functionality for each channel
+  ledcAttach(motorSpeedLeftPin, frequency, resolution);
+  ledcAttach(motorSpeedRightPin, frequency, resolution);
+
+  // Setup the motor direction pins
+  pinMode(motorDirectionLeftAPin, OUTPUT);
+  pinMode(motorDirectionLeftBPin, OUTPUT);
+  pinMode(motorDirectionRightAPin, OUTPUT);
+  pinMode(motorDirectionRightBPin, OUTPUT);
+}
+
+long distanceLeft = 0;
+long distanceRight = 0;
+float speedRequestLeft = 0;
+float speedRequestRight = 0;
+float currentSpeedLeft = -999;
+float currentSpeedRight = -999;
+
+void loop() 
+{
+  // Get the average distance traveled on each side
+  distanceLeft += ( encoder_left_back.readAndReset() + encoder_left_front.readAndReset() ) / 2;
+  distanceRight += ( encoder_right_back.readAndReset() + encoder_right_front.readAndReset() ) / 2;
+
+  if(speedRequestLeft != currentSpeedLeft)
+  {
+    currentSpeedLeft = speedRequestLeft;
+    updateSpeedController(motorSpeedLeftPin, motorDirectionLeftAPin, motorDirectionLeftBPin, currentSpeedLeft);
+  }
+
+  if(speedRequestRight != currentSpeedRight)
+  {
+    currentSpeedRight = speedRequestRight;
+    updateSpeedController(motorSpeedRightPin, motorDirectionRightAPin, motorDirectionRightBPin, currentSpeedRight);
+  }
+}
+
+void updateSpeedController(int pin, int a, int b, float speed)
+{
+  // Write the new speed. Since we configured the channel with 8bits, the % has to be changed to a 0-255 scale
+  // We should check ledcFade to smooth out the transitions, specially on start/stop
+  ledcWrite(pin, round(abs(speed) * 255.0 / 100.0));
+
+  // Write the direction
+  if(speed > 0)
+  {
+    digitalWrite(a, HIGH);
+    digitalWrite(b, LOW);
+  }
+  else if(speed < 0)
+  {
+    digitalWrite(a, LOW);
+    digitalWrite(b, HIGH);
+  }
+  else
+  {
+    digitalWrite(a, LOW);
+    digitalWrite(b, LOW);
+  }
+}
+
+// function that executes whenever data is requested by master
+// this function is registered as an event, see setup()
+void requestEvent() 
+{
+  long distances[2] = {distanceLeft, distanceRight};
+  distanceLeft = distanceRight = 0;
+
+  Wire.write((unsigned char*)&distances, sizeof(long) * 2);
+}
+
+// function that executes whenever data is received from master
+// this function is registered as an event, see setup()
+void receiveEvent(int howMany)
+{
+  if(howMany != 8) // 2 float
+    return; // shouldn't happen
+  
+  unsigned char buff[8];
+  int read = 0;
+  while(Wire.available()) // loop through all but the last
+  {
+    *(buff + read) = Wire.read(); // receive 1 byte;
+    ++read;
+  }
+  
+  if(read != howMany)
+    return; // shouldn't happen
+
+  speedRequestLeft = ((float*)buff)[0];
+  speedRequestRight = ((float*)buff)[1];
+}
---
+++ src/rover_esp32/src/home_rover.cpp
@@ -0,0 +1,54 @@
+#include <rclcpp/rclcpp.hpp>
+#include "std_msgs/msg/int32.hpp"
+#include <wiringPi.h>
+#include <wiringPiI2C.h>
+#include <unistd.h>
+
+class rover_esp32 : public rclcpp::Node {
+public:
+    rover_esp32() : Node("rover_esp32"), i2cAddress(2) {
+        this->declare_parameter<int>("frequency", 50);
+        int timer_frequency = this->get_parameter("frequency").as_int();
+
+        i2c_device = wiringPiI2CSetup(i2cAddress);
+        if (i2c_device == -1) {
+            throw std::runtime_error("Failed to initialize I2C.");
+        }
+
+        publisher_leftEncDist = this->create_publisher<std_msgs::msg::Int32>("left_enc_dist", 10);
+        publisher_rightEncDist = this->create_publisher<std_msgs::msg::Int32>("right_enc_dist", 10);
+
+        timer_ = this->create_wall_timer(
+            std::chrono::seconds(1) / timer_frequency,
+            std::bind(&rover_esp32::timerCallback, this)
+        );
+    }
+
+private:
+    void timerCallback() {
+        
+        int32_t speeds[2];
+        wiringPiI2CRawRead(i2c_device, (uint8_t*)speeds, sizeof(speeds));
+        
+        auto message = std_msgs::msg::Int32();
+        message.data = speeds[0];
+        publisher_leftEncDist->publish(message);
+
+        message.data = speeds[1];
+        publisher_rightEncDist->publish(message);
+    }
+
+    int i2cAddress;
+    int i2c_device;
+    rclcpp::Publisher<std_msgs::msg::Int32>::SharedPtr publisher_leftEncDist;
+    rclcpp::Publisher<std_msgs::msg::Int32>::SharedPtr publisher_rightEncDist;
+    rclcpp::TimerBase::SharedPtr timer_;
+};
+
+int main(int argc, char **argv) {
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<rover_esp32>();
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+    return 0;
+}
